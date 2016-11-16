bb59587fc3919989d43b7bb79e65cd46e11f56e8
adhe223 <adhe223@g.uky.edu>
2016-11-15 21:59:23 -0700

    Initialize Spring project

---
+++ src/main/java/com/thefantasyhof/HofWebServerApplication.java
@@ -0,0 +1,12 @@
+package com.thefantasyhof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HofWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HofWebServerApplication.class, args);
+	}
+}
