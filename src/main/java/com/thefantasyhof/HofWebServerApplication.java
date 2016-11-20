1eb201dcc48962138be6eb51a7b317fee780c6a7
adhe223 <adhe223@g.uky.edu>
2016-11-19 23:22:07 -0700

    Skeleton project working

--- src/main/java/com/thefantasyhof/HofWebServerApplication.java
+++ src/main/java/com/thefantasyhof/HofWebServerApplication.java
@@ -2,11 +2,22 @@ package com.thefantasyhof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HofWebServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HofWebServerApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(HofWebServerApplication.class, args);
+
+		System.out.println("Let's inspect the beans provided by spring boot");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 }
