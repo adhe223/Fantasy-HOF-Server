1651b89aa4d7b0258418a6f5d1d4e5154d55c476
adhe223 <adhe223@g.uky.edu>
2016-12-30 15:26:10 -0700

    Get thymeleaf hooked up

--- src/main/java/com/thefantasyhof/HofWebServerController.java
+++ src/main/java/com/thefantasyhof/HofWebServerController.java
@@ -1,38 +1,12 @@
 package com.thefantasyhof;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.apache.commons.io.IOUtils;
 
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.URL;
-import java.net.URLConnection;
-
-@RestController
+@Controller
 public class HofWebServerController {
-    @RequestMapping("/")
-    public String index() {
-        return "/html/index.html";
-    }
-
-    @RequestMapping("/getJSON")
-    public String getJSON() {
-        String url = "https://agile-badlands-78491.herokuapp.com/getLeagueDataJSON?leagueId=44169";
-        String charset = "UTF-8";
-
-        try {
-            URLConnection connection = new URL(url).openConnection();
-            InputStream response = new URL(url).openStream();
-
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(response, writer);
-            String responseString = writer.toString();
-
-            DataConsumer dc = new DataConsumer(responseString);
-            dc.tabulateData();
-
-            return "";
-        } catch (Exception e){ return "HTTP GET Failed!"; }
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
     }
 }
--- src/main/java/com/thefantasyhof/Utilities.java
+++ src/main/java/com/thefantasyhof/Utilities.java
@@ -1,8 +1,31 @@
 package com.thefantasyhof;
 
-/**
- * Created by zaber on 11/21/2016.
- */
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
 public final class Utilities {
     private Utilities() {}
+
+    public String getJSON() {
+        String url = "https://agile-badlands-78491.herokuapp.com/getLeagueDataJSON?leagueId=44169";
+        String charset = "UTF-8";
+
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            InputStream response = new URL(url).openStream();
+
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(response, writer);
+            String responseString = writer.toString();
+
+            DataConsumer dc = new DataConsumer(responseString);
+            dc.tabulateData();
+
+            return "";
+        } catch (Exception e){ return "HTTP GET Failed!"; }
+    }
 }
