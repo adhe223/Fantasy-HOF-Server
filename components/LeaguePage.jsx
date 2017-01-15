2a581e18feffeb0fe7bd46e81ea8944d7ea0b28c
adhe223 <adhe223@g.uky.edu>
2017-01-14 17:09:29 -0700

    Add total league points

--- components/LeaguePage.jsx
+++ components/LeaguePage.jsx
@@ -13,9 +13,12 @@ class LeaguePage extends React.Component {
 
         return(
             <div>
-            <BarChart chartName="championships" chartLabels={this.charts.championships.labels} chartDataset={this.charts.championships.dataset} chartDatasetLabel={this.charts.championships.datasetLabel} chartWidth={this.chartWidth}/>
-            <BarChart chartName="runnerUps" chartLabels={this.charts.runnerUps.labels} chartDataset={this.charts.runnerUps.dataset} chartDatasetLabel={this.charts.runnerUps.datasetLabel} chartWidth={this.chartWidth}/>
-            <BarChart chartName="leaguePointsByYear" chartLabels={this.charts.leaguePointsByYear.labels} chartDataset={this.charts.leaguePointsByYear.dataset} chartDatasetLabel={this.charts.leaguePointsByYear.datasetLabel} chartWidth={this.chartWidth}/>
+                <BarChart chartName="championships" chartLabels={this.charts.championships.labels} chartDataset={this.charts.championships.dataset} chartDatasetLabel={this.charts.championships.datasetLabel} chartWidth={this.chartWidth}/>
+                <BarChart chartName="runnerUps" chartLabels={this.charts.runnerUps.labels} chartDataset={this.charts.runnerUps.dataset} chartDatasetLabel={this.charts.runnerUps.datasetLabel} chartWidth={this.chartWidth}/>
+                <BarChart chartName="leaguePointsByYear" chartLabels={this.charts.leaguePointsByYear.labels} chartDataset={this.charts.leaguePointsByYear.dataset} chartDatasetLabel={this.charts.leaguePointsByYear.datasetLabel} chartWidth={this.chartWidth}/>
+                <ul>
+                    <li key="totalLeaguePoints">Total League Points: {this.charts.totalLeaguePoints}</li>
+                </ul>
             </div>
         );
     }
