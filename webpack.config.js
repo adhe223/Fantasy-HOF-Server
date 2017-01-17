2fb7236401ede119c7b6d24881f91794c1826601
adhe223 <adhe223@g.uky.edu>
2017-01-16 19:41:09 -0700

    Prod uses minified react

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,12 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
